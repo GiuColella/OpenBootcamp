a7e83641124a12eaf8b26970c178c277f9c99ec4
Giuliano Colella <colellag47@gmail.com>
2022-11-29 20:44:53 -0300

    commit main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int resultado = Suma(5,5,5);
+
+        System.out.println(resultado);
+
+        System.out.println("-------------------------------------");
+
+        Coche miCoche = new Coche();
+        miCoche.aumentarPuertas();
+
+        System.out.println(miCoche.puertas);
+
     }
+    public static int Suma (int x, int y, int z) {
+
+        return x + y + z;
+    }
+
+    static class Coche{
+        int puertas = 4;
+
+        public int aumentarPuertas (){
+            puertas = puertas + 1;
+
+            return puertas;
+        }
+
+    }
+
 }
